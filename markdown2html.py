e3f03090f8716ff981f3b980b4fcd3a35ec6f0ac
mmolalekan <muhayminolalekan@yahoo.com>
2024-03-22 16:49:24 +0100

    markdwon project

--- markdown2html.py
+++ markdown2html.py
@@ -2,17 +2,19 @@
 """
 Converts Markdown to HTML.
 """
-import sys
-import os
+
+if __name__ == "__main__":
+    import sys
+    import os
 
 
-if len(sys.argv) < 2:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
+    if len(sys.argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
 
-if not os.path.exists(sys.argv[1]):
-    print(f"Missing {sys.argv[1]}", file=sys.stderr)
-    sys.exit(1)
+    if not os.path.exists(sys.argv[1]):
+        print(f"Missing {sys.argv[1]}", file=sys.stderr)
+        sys.exit(1)
 
-markdown_file = sys.argv[1]
-output_file = sys.argv[2]
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
