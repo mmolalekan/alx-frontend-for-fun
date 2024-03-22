0446243efda05807beff58643098301d35abf253
mmolalekan <muhayminolalekan@yahoo.com>
2024-03-22 17:56:47 +0100

    task 1 done

--- markdown2html.py
+++ markdown2html.py
@@ -3,6 +3,13 @@
 Converts Markdown to HTML.
 """
 
+def headings(line):
+    count = 0
+    for i in line:
+        if i == "#":
+            count = count + 1
+    return f"<h{count}>{line[count + 1:-1]}</h{count}>\n"
+
 if __name__ == "__main__":
     import sys
     import os
@@ -14,8 +21,16 @@ if __name__ == "__main__":
     elif not os.path.exists(sys.argv[1]):
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
         sys.exit(1)
-    else:
-        exit(0)
 
-    # markdown_file = sys.argv[1]
-    # output_file = sys.argv[2]
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+    converted = ""
+    with open(markdown_file, 'r') as input_file:
+        input = input_file.readlines()
+        for line in input:
+            if line.startswith("#"):
+                converted = converted + headings(line)
+                 
+
+    with open(os.path.exists(sys.argv[1]), 'a') as output_file:
+            output_file.write(converted)
